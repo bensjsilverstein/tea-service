5cf7ba2f9098ff2c60eb74bbf861d5f76753fdcb
Ben Silverstein <benjsilverstein@comcast.net>
2022-11-04 08:48:01 -0600

    Add route for customer subcription index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post 'subscribe', to: 'customers/subscriptions#create'
       post 'subscriptions/cancel', to: 'customers/subscriptions#update'
+      get 'customer/subscriptions', to: 'customers/subscriptions#index'
     end
   end
 end
